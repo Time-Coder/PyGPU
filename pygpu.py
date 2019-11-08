11f26655c7db90ccdae6e3faa9cd961217a5001f
Time-Coder <binghuiwang5@gmail.com>
2019-11-09 04:04:35 +0800

    recover to not support mat

--- pygpu.py
+++ pygpu.py
@@ -112,12 +112,7 @@ class GPU():
 			exit(-1)
 
 		self._clear_args()
-		for i, host_arg in enumerate(args):
-			if isinstance(host_arg, np.ndarray):
-				origine_shape = host_arg.shape
-				host_arg = host_arg.flatten()
-				host_arg = np.reshape(host_arg, origine_shape)
-				
+		for i, host_arg in enumerate(args):				
 			device_arg = self._type_transform(host_arg, self._cl_typenames[i+1])
 			self._default_args.append(device_arg)
 			self._kernel.set_arg(i+1, device_arg)
